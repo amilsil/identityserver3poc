f90739b5fe87353a0c6c89f95ac9055fcac6b51f
amilsil <amilsil@gmail.com>
2015-11-17 16:09:21 +1100

    Basic IdentityServer [with no certifications for security]

---
+++ IdentityServer/IdentityServer/Scopes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IdentityServer3.Core.Models;
+
+namespace IdentityServer
+{
+    public static class Scopes
+    {
+        public static IEnumerable<Scope> Get()
+        {
+            return new List<Scope> {
+                StandardScopes.OpenId,
+                StandardScopes.Profile,
+                StandardScopes.Email
+            };
+        }
+    }
+
+}
---
+++ IdentityServer/IdentityServer/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IdentityServer3.Core.Configuration;
+using Owin;
+
+namespace IdentityServer
+{
+    public sealed class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Map(
+                "/core",
+                coreApp => coreApp.UseIdentityServer(new IdentityServerOptions
+                {
+                    SiteName = "Standalone Identity Server",
+                    //SigningCertificate = Cert.Load(),
+                    Factory = new IdentityServerServiceFactory()
+                        .UseInMemoryClients(Clients.Get())
+                        .UseInMemoryScopes(Scopes.Get())
+                        .UseInMemoryUsers(Users.Get()),
+
+                    RequireSsl = false
+                }));
+        }
+    }
+}
