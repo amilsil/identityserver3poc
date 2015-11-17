8440862a90c04e8d557d6b0f8884ade0fa7d6634
amilsil <amilsil@gmail.com>
2015-11-17 17:14:40 +1100

    Working with a Form POST client

--- IdentityServer/IdentityServer/Startup.cs
+++ IdentityServer/IdentityServer/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using IdentityServer3.Core.Configuration;
+using IdentityServer3.Core.Logging;
 using Owin;
 
 namespace IdentityServer
@@ -16,7 +17,7 @@ namespace IdentityServer
                 coreApp => coreApp.UseIdentityServer(new IdentityServerOptions
                 {
                     SiteName = "Standalone Identity Server",
-                    //SigningCertificate = Cert.Load(),
+                    SigningCertificate = Cert.Load(),
                     Factory = new IdentityServerServiceFactory()
                         .UseInMemoryClients(Clients.Get())
                         .UseInMemoryScopes(Scopes.Get())
